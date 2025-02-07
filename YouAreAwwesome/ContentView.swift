ba59ed50bb431e819b2826bce0d82241fe02d3f7
Vincent Cina <vcina@me.com>
2025-02-07 15:55:38 -0500

    Initial Commit

---
+++ YouAreAwwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwwesome
+//
+//  Created by VINCENT CINA on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwwesome/YouAreAwwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwwesomeApp.swift
+//  YouAreAwwesome
+//
+//  Created by VINCENT CINA on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
