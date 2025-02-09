fdba5c125d5a3f4427d2e790497a49f6d3fad1e3
Vincent Cina <vcina@me.com>
2025-02-09 13:46:20 -0500

    Professor Gallauger SwiftUI 2025 course lesson 1,6. Replace FL statement with ternary operator

--- YouAreAwwesome/ContentView.swift
+++ YouAreAwwesome/ContentView.swift
@@ -31,13 +31,8 @@ struct ContentView: View {
                 let imageString1 = "hand.thumbsup"
                 let imageString2 = "sun.max.fill"
                 
-                if message == message1 {
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
-                }
+                message = message == message1 ? message2 : message1
+                imageString = message == message2 ? imageString2 : imageString1
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
