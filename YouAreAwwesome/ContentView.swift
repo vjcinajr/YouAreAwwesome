45340ba29cb090b5eacaf5e90e27aac19ec8f5c7
Vincent Cina <vcina@me.com>
2025-02-09 12:39:12 -0500

    Added if … else statements and used let constants to make a toggle button. Professor Gallauger SwiftUI 2025 course lesson 1.5

--- YouAreAwwesome/ContentView.swift
+++ YouAreAwwesome/ContentView.swift
@@ -9,36 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
     
     var body: some View {
+
         
         VStack {
-            
             Spacer()
-            
-            Image(systemName: "swift")
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
             Spacer()
-            Spacer()
-            HStack {
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
-                Button("Breat") {
-                    message = "Awesome!"
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else {
+                    message = message1
+                    imageString = imageString1
                 }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            
-            .fontWeight(.ultraLight)
             .tint(.orange)
         }
         .padding()
