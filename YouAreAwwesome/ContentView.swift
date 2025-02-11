12c3b9b9c5dfe47870170da437027f54e1c346da
Vincent Cina <vcina@me.com>
2025-02-10 19:38:48 -0500

    Make sure neither messages or images ever repeat by using a repeat wild loop

--- YouAreAwwesome/ContentView.swift
+++ YouAreAwwesome/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var lastMessageNumber: Int = -1
+    @State private var lastImageNumber: Int = -1
+    
     var body: some View {
         VStack {
             Text(message)
@@ -37,8 +40,22 @@ struct ContentView: View {
                                 "You are Amazing!",
                                 "You are Superb!",
                                 "You are Fantastic!"]
-                message = messages[Int.random(in: 0..<messages.count)]
-                imageName = "image\(Int.random(in: 0...9))"
+                
+                // generate a new message number not equal to the last message number
+                var nextMessageNumber: Int
+                repeat {
+                    nextMessageNumber = Int.random(in: 0..<messages.count)
+                } while lastMessageNumber == nextMessageNumber
+                message = messages[nextMessageNumber]
+                lastMessageNumber = nextMessageNumber
+                
+                // generate a new image number not equal to the last image number
+                var nextImageNumber: Int
+                repeat {
+                    nextImageNumber = Int.random(in: 0...9)
+                } while lastImageNumber == nextImageNumber
+                imageName = "image\(nextImageNumber)"
+                lastImageNumber = nextImageNumber
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
