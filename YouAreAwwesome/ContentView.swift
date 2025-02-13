87eb1d64d9ccc61da37b6625fb2dc92d2f79cb32
Vincent Cina <vcina@me.com>
2025-02-13 16:48:22 -0500

    Added stop sound toggle nd ensure only one sound is playing at any time

--- YouAreAwwesome/ContentView.swift
+++ YouAreAwwesome/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var lastImageNumber: Int = -1
     @State private var lastSoundNumber: Int = -1
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundIsOn: Bool = true
     
     let numberOfImages: Int = 10    // Ten images labeled image0 - image9
     let numberOfSounds: Int = 5     // Ten images labeled sound0 - sound4
@@ -39,35 +40,47 @@ struct ContentView: View {
                 .shadow(radius: 30)
                 .animation(.default, value: imageName)
             Spacer()
-            Button("Show Message") {
-                let messages = ["You are Great",
-                                "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
-                                "When the Genius Bar Needs Help, They Call You!",
-                                "You are Awesome!",
-                                "Fabulous? That's You!",
-                                "You are Amazing!",
-                                "You are Superb!",
-                                "You are Fantastic!"]
-                
-                // show a new random message
-                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber,
-                                                       upperBounds: messages.count-1)
-                message = messages[lastMessageNumber]
-                
-                
-                // show a new random image
-                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber,
-                                                     upperBounds: numberOfImages-1)
-                imageName = "image\(lastImageNumber)"
-                
-                
-                // play a new sound
-                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber,
-                                                     upperBounds: numberOfSounds-1)
-                playSound(soundName: "sound\(lastSoundNumber)")
+            HStack {
+                Text("Sound On")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) {
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
+                Spacer()
+                Button("Show Message") {
+                    let messages = ["You are Great",
+                                    "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
+                                    "When the Genius Bar Needs Help, They Call You!",
+                                    "You are Awesome!",
+                                    "Fabulous? That's You!",
+                                    "You are Amazing!",
+                                    "You are Superb!",
+                                    "You are Fantastic!"]
+                    
+                    // show a new random message
+                    lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber,
+                                                           upperBounds: messages.count-1)
+                    message = messages[lastMessageNumber]
+                    
+                    
+                    // show a new random image
+                    lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber,
+                                                         upperBounds: numberOfImages-1)
+                    imageName = "image\(lastImageNumber)"
+                                        
+                    // play a new sound
+                    lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber,
+                                                         upperBounds: numberOfSounds-1)
+                    if soundIsOn {
+                        playSound(soundName: "sound\(lastSoundNumber)")
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
             }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
         }
         .padding()
     }
@@ -82,6 +95,9 @@ struct ContentView: View {
     
     func playSound(soundName: String) {
         // play the sound
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
         guard let soundFile = NSDataAsset(name: soundName) else {
             print("😡 Could not read file named \(soundName)")
             return
